e54da0a9c3ebf14e2468da16fa3431de662c5e87
Jonas Müller <jonmue@uni-bremen.de>
2024-05-25 17:20:02 +0200

    added random changing of color, size and mass of the balls

--- app/main.f90
+++ app/main.f90
@@ -5,6 +5,8 @@ program main
   use class_Ball
   
   implicit none (type, external)
+
+  real :: random_num
   ! loop variables
   integer :: i, j
 
@@ -16,6 +18,7 @@ program main
 
   real :: BALL_RADII(10) = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
   real :: BALL_MASSES(10) = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  type(color_type) :: BALL_COLORS(10) = [RED, BLUE, GREEN, YELLOW, ORANGE, PINK, PURPLE, VIOLET, BROWN, MAGENTA]
 
   ! raylib winodw settings
   integer, parameter :: SCREEN_WIDTH = 600
@@ -23,9 +26,12 @@ program main
   integer, parameter :: BOUNDS(2) = [SCREEN_WIDTH, SCREEN_HEIGHT]
   real, parameter :: START_COORDS(2) = [SCREEN_WIDTH/2, 600]  
 
+  character(len=100) :: particle_number
 
 
-  balls = [Ball(color=RED, radius=BALL_RADII(5), mass=BALL_MASSES(1), coords=START_COORDS, velocity=[0.0, 0.0],gravity=900)]
+
+  !balls = [Ball(color=RED, radius=BALL_RADII(5), mass=BALL_MASSES(1), coords=START_COORDS, velocity=[0.0, 0.0],gravity=900)]
+  allocate(balls(1))      
 
 
   ! Initialize window
@@ -37,7 +43,15 @@ program main
     if ( is_key_pressed(KEY_UP) ) then
       balls(size(balls))%gravity = 900  
       balls(size(balls))%coords(2) = balls(size(balls))%coords(2) + balls(size(balls))%radius * 2
-      balls = [balls, Ball(color=BLUE,radius=50.0, mass=1.0, coords=[SCREEN_WIDTH/2, 50], velocity=[0.0, 0.0], gravity=0)]
+      call random_number(random_num)
+      random_num = random_num * 9 + 1
+      print *, int(random_num)
+      balls = [balls, Ball(color=BALL_COLORS(int(random_num)),&
+              radius=BALL_RADII(int(random_num)), &
+              mass=BALL_MASSES(int(random_num)),&
+              coords=[SCREEN_WIDTH/2, 50],&
+              velocity=[0.0, 0.0],&
+              gravity=0)]
     elseif ( is_key_pressed(KEY_DOWN) ) then
       balls = [balls(:size(balls)-2), balls(size(balls))]  ! remove last ball
     end if
@@ -61,6 +75,8 @@ program main
          call balls(i)%draw
         end do
     call draw_fps(10, 10)
+    write(particle_number, '(I10)') size(balls)
+    call draw_text("NUMBER OF PARTICLES" // particle_number // c_null_char, 10, 30, 10, GRAY)
     call end_drawing()
   end do
   call close_window()
